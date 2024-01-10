97a014f61a7b94c817387d9e5140619358458710
Steven Gong <gonghaima@hotmail.com>
2024-01-11 00:53:22 +1100

    funcational delegate

--- FuncActionPredicateExamples/Program.cs
+++ FuncActionPredicateExamples/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        MathClass mathClass = new MathClass();
+
+        Func<int, int, int> calc = mathClass.Sum;
+
+        int result = calc(1,2);
+        Console.WriteLine($"Result: {result}");
+    }
+}
+
+public class MathClass
+{
+    public int Sum(int a, int b)
+    {
+        return a + b;
     }
 }
 
